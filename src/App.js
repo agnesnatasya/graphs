aadf67a8e9401e40f5fbdc344466e7ace67ab050
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-03 01:13:09 +0800

    Create routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SignUp } from './components/SignUp';
+import { PostForm } from './components/PostForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Route exact path="/submit" component={<Result />} />
+        <Route exact path="/" component={<PythonForm />} />
+      </Container>
+    );
+  }
 }
 
+
 export default App;
+
+
+
+
+
