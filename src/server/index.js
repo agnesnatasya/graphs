a26c95fbe51ad134e10b8b250398ef823e83c643
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-04 11:07:32 +0800

    Add logic to read the python code, execute, trace and give the coverage result to the frontend

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const { spawn } = require('child_process');
 const app = express()
 const port = 3000
-app.get('/submit', (req, res) => {
+const router = express.Router();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/submit', (req, res) => {
+  code_value = (req.body.code);
+  console.log(code_value);
   /*
   var dataToSend;
   // spawn new child process to call the python script
@@ -21,15 +28,21 @@ app.get('/submit', (req, res) => {
   });
   */
   const { exec } = require('child_process');
-  var dataToSend;
+  const python_first = spawn('python', ['script0.py', code_value]);
+  /*python_first.stdout.on('data3', function (data3) {
+    console.log('Lol this is the first result ...');
+    console.log(data3.toString());
+  });*/
+
   exec('python3  -m trace --count -C . script1.py', (err, stdout, stderr) => {
     if (err) {
       //some err occurred
-      console.error(err)
+      console.error(err);
     } else {
       // the *entire* stdout and stderr (buffered)
       // spawn new child process to call the python script
       const python = spawn('python', ['script3.py']);
+      var dataToSend;
       // collect data from script
       python.stdout.on('data', function (data) {
         console.log('Pipe data from python script ...');
@@ -37,16 +50,13 @@ app.get('/submit', (req, res) => {
       });
       // in close event we are sure that stream from child process is closed
       python.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code}`);
+        console.log('child process close all stdio with code ${code}');
         // send data to browser
-        res.send(dataToSend)
+        console.log(dataToSend);
+        res.send(dataToSend);
       });
     }
   });
-  return dataToSend
-
-
 })
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
