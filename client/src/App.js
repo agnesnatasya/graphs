65c413e6e0094e5e023f496b375f7a32a88d78d1
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-07-07 01:10:36 +0800

    Add Navbar for logo and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { PythonForm } from './components/PythonForm';
 
+const NavBar = () => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 class App extends Component {
 
   render() {
     return (
       <div>
+        <NavBar />
         <Route exact path="/submit" />
         <Route exact path="/" component={PythonForm} />
       </div >
