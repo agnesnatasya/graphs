75d1b1993ddce70a5492713d1b09a47fce69bc43
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-01 16:32:03 +0800

    Remove unneccessary function files

--- backend/function/function2.py
+++
@@ -1,8 +0,0 @@
-def f(x):
-  for i in range(x):
-    for i in range(x):
-      print("A")
-import sys
-if __name__ == '__main__':
-	input_int = sys.argv[1]
-	f(int(input_int))
--- backend/function/function3000.py
+++
@@ -1,7 +0,0 @@
-def f(x):
-  for i in range(6):
-    print(x)
-import sys
-if __name__ == '__main__':
-	input_int = sys.argv[1]
-	f(int(input_int))
