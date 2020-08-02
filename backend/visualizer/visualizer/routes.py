f10409a12d7800ea72cfcae2ba0f0b7ac998e3f5
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-03 03:35:58 +0800

    Set route submit to return coord and equation as json object

--- backend/visualizer/visualizer/routes.py
+++ backend/visualizer/visualizer/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 from visualizer import app
 from .utils import function_writer, regression, cover_scrape
 from .functions import f1
@@ -6,18 +6,18 @@ import trace
 import sys
 import os
 
-n_list = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+x_list = [1, 2, 4, 8, 16, 32, 64, 128, 256]
 
 @app.route('/submit', methods=['POST'])
 def hello_world():
     print("AAAA")
-    code = request.body.code
+    code = request.get_json().get('code')
     y_list = []
-    for n in n_list:
+    for n in x_list:
         function_writer.write_function(code, str(n))
         os.system(f'python3 -m trace --count -C ./visualizer/functions ./visualizer/functions/f{n}.py {n}')
         result = cover_scrape.scrape_cover(str(n))
         y_list.append(result)
-    result = regression.approximate_time_complexity(n_list, y_list)
-    print(result)
-    return jsonify({ "data": function_writer.read_function()})
+    equation = regression.approximate_time_complexity(x_list, y_list)
+    coord = [{"x": coord[0], "y": coord[1]} for coord in zip(x_list, y_list)]
+    return jsonify({"equation": equation, "coord": coord})
