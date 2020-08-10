9ec0a8b6dc2884bc994a57e43aeae3359a339bb3
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-11 00:29:13 +0800

    Add title into a different component

---
+++ client/src/components/HeaderBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+export const HeaderBar = () => (
+  <Navbar bg="light" expand="lg">
+    <Navbar.Brand href="#home">TIME COMPLEXITY VISUALIZER</Navbar.Brand>
+  </Navbar>
+);
