4ffa7b72bce84424177326ef81b02f7c980be205
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-11 00:29:28 +0800

    Remove routing from app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import { PythonForm } from './components/PythonForm';
+import { HeaderBar } from "./components/HeaderBar";
+import { CodeForm } from './components/CodeForm';
 
-const NavBar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">TIME COMPLEXITY VISUALIZER</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-)
+
 class App extends Component {
 
   render() {
     return (
       <div>
-        <NavBar />
-        <Route exact path="/submit" />
-        <Route exact path="/" component={PythonForm} />
+        <HeaderBar />
+        <CodeForm />
       </div >
     );
   }
