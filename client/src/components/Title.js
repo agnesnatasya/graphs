63bb3164a20d3e43123dc251700cd68496ee367c
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-30 19:33:44 +0800

    Add the title component and styling

---
+++ client/src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Title.css";
+
+export function Title() {
+    return (
+        <div className="header-bar">
+            <span className="title">Time Complexity Visualizer</span>
+        </div>
+    );
+}
