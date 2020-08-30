466aa7a7cd903240629b9a82625ea95ef4e27504
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-08-30 19:37:27 +0800

    Delete HeaderBar component

--- client/src/components/HeaderBar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
-
-export const HeaderBar = () => (
-  <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="#home">TIME COMPLEXITY VISUALIZER</Navbar.Brand>
-  </Navbar>
-);
