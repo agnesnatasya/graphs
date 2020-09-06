9e63c92234fb6cd3b74f129e1a36920deb736b45
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-07 02:32:22 +0800

    Change the url to fetch data from

--- src/components/CodeForm.js
+++ src/components/CodeForm.js
@@ -28,7 +28,6 @@ export class CodeForm extends React.Component {
   }
 
   handleStateOnResponse = (text) => {
-    console.log('asf');
     this.setState({
       coord: JSON.parse(text).coord,
       exponential: JSON.parse(text).equation.exponential,
