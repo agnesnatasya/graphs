ad0033d564be4d1be0aa14506d0bc07078251ca3
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-13 14:37:40 +0800

    Addheaders to allow cors

--- amplify/backend/function/timeComplexity/src/index.py
+++ amplify/backend/function/timeComplexity/src/index.py
@@ -2,12 +2,27 @@ import function_writer
 import regression
 import cover_scrape
 import os
+import json
 
 
 def handler(event, context):
     x_list = [1, 2, 4, 8, 16, 32, 64, 128, 256]
     print(event)
     print(event.get('body', {}))
+    if not event.get('body'):
+        return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers':
+                'Content-Type',
+            'Access-Control-Allow-Origin':
+                '*',
+            'Access-Control-Allow-Methods':
+                'OPTIONS,POST,GET'
+        },
+        'body': json.dumps([])
+    }
+
     code = event.get('body', {}).get('code')
     y_list = []
     for n in x_list:
@@ -20,4 +35,16 @@ def handler(event, context):
     equation = regression.approximate_time_complexity(x_list, y_list)
     coord = [{"x": coord[0], "y": coord[1]} for coord in zip(x_list, y_list)]
     print({"equation": equation, "coord": coord})
-    return {"equation": equation, "coord": coord}
+    body = json.dumps({"equation": equation, "coord": coord})
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers':
+                'Content-Type',
+            'Access-Control-Allow-Origin':
+                '*',
+            'Access-Control-Allow-Methods':
+                'OPTIONS,POST,GET'
+        },
+        'body': body
+    }
