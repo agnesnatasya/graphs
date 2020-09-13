6b3fede19a69b26cb9067ee4d8ade22493df2707
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-13 12:12:35 +0800

    ADd configuration for AWS Amplify in main.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Title } from "./components/Title";
 import { CodeForm } from './components/CodeForm';
+import Amplify, { API } from "aws-amplify";
 import awsconfig from "./aws-exports";
 
 Amplify.configure(awsconfig);
