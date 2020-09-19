c588665ce8a8213d944d05ec11b5d7532de57976
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-20 02:51:06 +0800

    Refactor Explanation component to a different file:

---
+++ src/components/Explanation.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import "./Explanation.css";
+
+export function Explanation() {
+  return (
+    <div>
+        <h6>
+            Write down your algorithm in the box provided and predict its time
+            complexity.
+          </h6>
+          Rules:
+          <ul>
+            <li>The algorithm should be written in Python </li>
+            <li>
+              The algorithm should be written in the form of <pre>def f(n)</pre>
+              , where n represents the input size to the algorithm.
+            </li>
+          </ul>
+    </div>
+  );
+}
