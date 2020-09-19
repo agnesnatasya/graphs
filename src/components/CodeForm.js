0d683dda08834836eaed76211c47764900c0b4ad
agnesnatasya <agnesnatasyawijaya@gmail.com>
2020-09-20 01:58:13 +0800

    Fix tab character in text area'

--- src/components/CodeForm.js
+++ src/components/CodeForm.js
@@ -4,9 +4,6 @@ import { chart } from './Chart.js';
 import { equation } from './Equation.js';
 import "./CodeForm.css";
 
-const apiName = 'timeComplexity';
-const path = '/time-complexity'; 
-
 export class CodeForm extends React.Component {
   constructor(props) {
     super(props);
@@ -18,22 +15,54 @@ export class CodeForm extends React.Component {
         logarithm: 0,
         isFetching: false
     };
+    this.textAreaRef = React.createRef();
     this.handleKeyDown = this.handleKeyDown.bind(this);
     this.handleOnClick = this.handleOnClick.bind(this);
     this.handleStateOnResponse = this.handleStateOnResponse.bind(this);
   }
 
   handleKeyDown = (event) => {
-    if (event.keyCode === 9) {
+    /*if (event.keyCode === 9) {
       event.preventDefault();
+
       var val = this.state.code,
         start = event.target.selectionStart,
         end = event.target.selectionEnd;
+      event.target.selectionEnd = end;
+      console.log(start);
+      console.log(end);
+      this.refs.input.selectionEnd = start + 1;
       this.setState(
         {
           "code": val.substring(0, start) + '  ' + val.substring(end)
         }
       )
+    }*/
+    // 'event.key' will return the key as a string: 'Tab'
+    // 'event.keyCode' will return the key code as a number: Tab = '9'
+    // You can use either of them
+    if (event.keyCode === 9) {
+      // Prevent the default action to not lose focus when tab
+      event.preventDefault();
+
+      // Get the cursor position
+      const { selectionStart, selectionEnd } = event.target;
+      console.log(selectionStart);
+      console.log(selectionEnd);
+      // update the state
+      this.setState(
+        (prevState) => ({
+          code:
+            prevState.code.substring(0, selectionStart) +
+            "  " + // '\t' = tab, size can be change by CSS
+            prevState.code.substring(selectionEnd),
+        }),
+        // update the cursor position after the state is updated
+        () => {
+          this.textAreaRef.current.selectionStart = this.textAreaRef.current.selectionEnd =
+            selectionStart + 2;
+        }
+      );
     }
   }
 
@@ -71,16 +100,20 @@ export class CodeForm extends React.Component {
     return (
       <Form>
         <Form.Group controlId="exampleForm.ControlTextarea1">
-              <h6>Write down your algorithm in the box provided and predict its time complexity.
-              </h6>
-                Rules:
-                  <ul>
-                    <li>The algorithm should be written in Python </li>
-                    <li>The algorithm should be written in the form of <pre>def f(n)</pre>, where n represents
-              the input size to the algorithm.
-                    </li>
-                  </ul>
+          <h6>
+            Write down your algorithm in the box provided and predict its time
+            complexity.
+          </h6>
+          Rules:
+          <ul>
+            <li>The algorithm should be written in Python </li>
+            <li>
+              The algorithm should be written in the form of <pre>def f(n)</pre>
+              , where n represents the input size to the algorithm.
+            </li>
+          </ul>
           <Form.Control
+            ref={this.textAreaRef}
             as="textarea"
             rows="15"
             placeholder="Write your code here"
